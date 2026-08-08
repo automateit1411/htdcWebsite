779ec51953ee4e10cc9a2da11960391bef25570b
automateit1411 <automateit1411@gmail.com>
2026-08-08 11:21:40 +0600

    Footer YouTube icon - use external image

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -960,10 +960,8 @@
                 @endif
                 @if(isset($settings) && $settings->youtube_url)
                 <a href="{{ $settings->youtube_url }}" target="_blank" class="group" aria-label="YouTube">
-                    <div class="w-9 h-9 rounded-full bg-[#FF0000] flex items-center justify-center group-hover:scale-110 group-hover:shadow-lg group-hover:shadow-red-500/50 transition-all duration-300">
-                        <svg class="w-5 h-5 text-white" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
-                        </svg>
+                    <div class="w-9 h-9 rounded-full bg-[#FF0000] flex items-center justify-center group-hover:scale-110 group-hover:shadow-lg group-hover:shadow-red-500/50 transition-all duration-300 overflow-hidden">
+                        <img src="https://images.icon-icons.com/195/PNG/256/YouTube_23392.png" alt="YouTube" class="w-5 h-5 object-contain">
                     </div>
                 </a>
                 @endif
